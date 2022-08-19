fa3c4b08402e618769f8081342d440df1a9dbd74
olliedirker <olliedirker@gmail.com>
2022-08-18 20:48:22 -0500

    attempt geocoding

--- public/javascript/address.js
+++ public/javascript/address.js
@@ -1,20 +1,101 @@
-import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
+// import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
 
-const { options } = require("../../controllers");
+// const { options } = require("../../controllers");
 
 
 
-    const autocomplete = new GeocoderAutocomplete(
-        document.getElementById("autocomplete"), 
-        '6ad86719e3024a59a955ca33eaeefacb', 
-            options.type={ 
-        country, state, city, postcode, street,  });
+//     const autocomplete = new GeocoderAutocomplete(
+//         document.getElementById("autocomplete"), 
+//         '6ad86719e3024a59a955ca33eaeefacb', 
+//             options.type={ 
+//         country, state, city, postcode, street,  });
 
-autocomplete.on('select', (location) => {
-// check selected location here 
+// autocomplete.on('select', (location) => {
+// // check selected location here 
+// });
+
+// autocomplete.on('suggestions', (suggestions) => {
+// // process suggestions here
+// });
+
+function addressAutocomplete(containerElement, callback, options) {
+	// create container for input element
+  const inputContainerElement = document.createElement("div");
+  inputContainerElement.setAttribute("class", "input-container");
+  containerElement.appendChild(inputContainerElement);
+
+  // create input element
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "text");
+  inputElement.setAttribute("placeholder", options.placeholder);
+  inputContainerElement.appendChild(inputElement);
+
+  const MIN_ADDRESS_LENGTH = 3;
+  const DEBOUNCE_DELAY = 300;
+
+  /* Process a user input: */
+  inputElement.addEventListener("input", function(e) {
+    const currentValue = this.value;
+
+    // Cancel previous timeout
+    if (currentTimeout) {
+      clearTimeout(currentTimeout);
+    }
+
+    // Cancel previous request promise
+    if (currentPromiseReject) {
+      currentPromiseReject({
+        canceled: true
+      });
+    }
+
+    // Skip empty or short address strings
+    if (!currentValue || currentValue.length < MIN_ADDRESS_LENGTH) {
+      return false;
+    }
+
+    /* Call the Address Autocomplete API with a delay */
+    currentTimeout = setTimeout(() => {
+    	currentTimeout = null;
+            
+      /* Create a new promise and send geocoding request */
+      const promise = new Promise((resolve, reject) => {
+        currentPromiseReject = reject;
+
+        // Get an API Key on https://myprojects.geoapify.com
+        const apiKey = "6ad86719e3024a59a955ca33eaeefacb";
+
+        var url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(currentValue)}&format=json&limit=5&apiKey=${apiKey}`;
+
+        fetch(url)
+          .then(response => {
+            currentPromiseReject = null;
+
+            // check if the call was successful
+            if (response.ok) {
+              response.json().then(data => resolve(data));
+            } else {
+              response.json().then(data => reject(data));
+            }
+          });
+      });
+
+      promise.then((data) => {
+        // here we get address suggestions
+        console.log(data);
+      }, (err) => {
+        if (!err.canceled) {
+          console.log(err);
+        }
+      });
+    }, DEBOUNCE_DELAY);
+  });  
+}
+
+
+addressAutocomplete(document.getElementById("autocomplete-container"), (data) => {
+  console.log("Selected option: ");
+  console.log(data);
+}, {
+	placeholder: "Enter an address here"
 });
-
-autocomplete.on('suggestions', (suggestions) => {
-// process suggestions here
-});
-
